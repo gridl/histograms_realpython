a62874bee896eb47f0a03be0ad4db78ace31198b
myrmica-habilis <michalportes1@gmail.com>
2020-01-14 23:28:05 +0100

    Add code from video #3

---
+++ lesson_3.py
@@ -0,0 +1,16 @@
+"""
+https://realpython.com/lessons/numpy-histograms/
+"""
+
+import numpy as np
+
+np.random.seed(444)
+np.set_printoptions(precision=3)
+
+d = np.random.laplace(loc=15, scale=3, size=500)
+
+hist, bin_edges = np.histogram(d)
+
+print(hist)
+print()
+print(bin_edges)
