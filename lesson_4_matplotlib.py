c783351a7fda7b73da89337a0bf6bafb60ceafef
myrmica-habilis <michalportes1@gmail.com>
2020-01-14 23:36:18 +0100

    Add code from video #4

---
+++ lesson_4_matplotlib.py
@@ -0,0 +1,24 @@
+"""
+https://realpython.com/lessons/matplotlib-and-pandas/
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(444)
+np.set_printoptions(precision=3)
+
+d = np.random.laplace(loc=15, scale=3, size=500)
+
+n, bins, patches = plt.hist(x=d, bins='auto', color='#0504aa',
+                            alpha=0.7, rwidth=0.85)
+
+plt.grid(axis='y', alpha=0.75)
+plt.xlabel('Value')
+plt.ylabel('Frequency')
+plt.title('My Very Own Histogram')
+plt.text(23, 24, r'$\mu=15, b=3$')
+maxfreq = n.max()
+plt.ylim(top=np.ceil(maxfreq / 10) * 10 if maxfreq % 10 else maxfreq + 10)
+
+plt.show()
